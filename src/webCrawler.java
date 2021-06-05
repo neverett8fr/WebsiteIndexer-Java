4d9e68fab94089d253bb72ff2d7156ba40ac44eb
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2021-06-05 08:15:04 +0100

    getFile function made, to read that big ol file

--- src/webCrawler.java
+++ src/webCrawler.java
@@ -1,5 +1,30 @@
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class webCrawler {
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         System.out.println("Hello World");
+        System.out.println(getFile("C:/Users/Nathan/Downloads/directory-list-2.3-big.txt").get(4));
+
+
+    }
+
+    public static List<String> getFile(String filePath){
+
+        try {
+            List<String> fileOutput;
+            fileOutput = Files.readAllLines(Paths.get(filePath),
+                    Charset.defaultCharset());
+
+            return fileOutput;
+        }
+        catch (Exception e){
+            return null;
+        }
+
     }
 }
