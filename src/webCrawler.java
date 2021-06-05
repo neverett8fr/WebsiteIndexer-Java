905b2acc9ef9db5ce582589e6869b2421f326e36
Nathan Everett <nathan.everett@students.plymouth.ac.uk>
2021-06-05 08:49:18 +0100

    almost finished, getAllDir, get if valid based on http response code, etc.

--- src/webCrawler.java
+++ src/webCrawler.java
@@ -1,17 +1,73 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.net.URL;
 
 public class webCrawler {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
-        System.out.println(getFile("C:/Users/Nathan/Downloads/directory-list-2.3-big.txt").get(4));
+        //"C:/Users/Nathan/Downloads/directory-list-2.3-big.txt"
+
+        // enter base website e.g. www.google.com, etc.
+        // load in text file with common directories
+        // search to see if website + "/directory(x)" valid
+
+        //List<String> dirList = getFile("C:/Users/Nathan/Downloads/directory-list-2.3-big.txt");
+        //for (int i = 0; i < dirList.size(); i++){
+        //    System.out.println(dirList.get(i));
+        //}
+
+        List<String> dirList = getFile("C:/Users/Nathan/Downloads/directory-list-2.3-big.txt");
+
+        List<String> allValidDir = getAllDir(dirList,"https://www.google.com");
+
+
 
 
     }
+
+    public static List<String> getAllDir(List<String> dictionary, String website){
+
+        List<String> output = null;
+
+        for (int i = 0; i < dictionary.size(); i++){
+            if(getDirExists(website, dictionary.get(i))){
+                output.add(dictionary.get(i));
+            }
+        }
+
+        return output;
+
+    }
+
+    public static boolean getDirExists(String base, String dir){
+
+        try{
+            URL urlTest = new URL(base + "/" + dir);
+
+            HttpURLConnection connection =  (HttpURLConnection)  urlTest.openConnection();
+            connection.setRequestMethod("HEAD");
+            connection.connect();
+
+            if (connection.getResponseCode() == 200){
+                return true;
+
+            }
+
+        }
+        catch (Exception e){
+            return false;
+        }
+
+        return false;
+
+    }
+
+
 
     public static List<String> getFile(String filePath){
 
